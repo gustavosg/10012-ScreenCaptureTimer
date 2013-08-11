6f3f19a76098aef7f9019f1e7c9e492efebd6bd8
Gustavo Souza Gonçalves <gustavo@geraes.net>
2013-08-11 12:59:39 -0300

    Criado ViewModels

---
+++ FrontEnd/10012-ScreenCaptureTimer/Views/Configuration/ConfigurationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace _10012_ScreenCaptureTimer.ViewModel.Configuration
+{
+    class ConfigurationViewModel
+    {
+    }
+}
