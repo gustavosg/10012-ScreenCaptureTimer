827747835fd9075a47e1e99a51454890d71d304d
Gustavo Souza Gonçalves <gustavo@geraes.net>
2013-08-16 23:07:42 -0300

    Ajuste de Core do sistema
    Adicionado classes para futuro ajuste. Classes que irão ajudar no
    desenvolvimento do sistema e que tem funções de nucleo de sistema, que
    serão utilizados por todo o mesmo.

---
+++ Core/GUI/MVVM/ViewModelHelpers/ViewModelHelpers/PropertyChanged.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ViewModelHelpers
+{
+    public class PropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+
+            if (handler == null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+    }
+}
